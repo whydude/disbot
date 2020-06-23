ea0a6c044bb925f74509a5cce66ac56a0833ed10
whydude <66397586+whydude@users.noreply.github.com>
2020-06-23 13:53:47 +0700

    Create whybot.py

---
+++ whybot.py
@@ -0,0 +1,21 @@
+from asyncio import sleep
+import discord
+from covid19 import *
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='=')
+
+
+@bot.command()
+async def covid(ctx):
+    embed = discord.Embed(title='Latest covid update')
+    embed.add_field(name = get_update_date(), value= get_data_covid())
+    embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def math(ctx,arg):
+    result = eval(arg)
+    await ctx.send(result)
+
+bot.run('NzIxMTgzNzY5MTI1NTg0OTI5.Xu8gog.BTtUDKyn8mw1BdPjKzgIG8bpwkI')
